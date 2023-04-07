79be16316c42e3fc1e0e340ec3f787eb0286e7bd
kkaiuu <kkaioxxavier2506@gmail.com>
2023-04-06 21:21:20 -0300

    terminei

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿Console.Write("Dó, "); Console.Beep(1320, 300);
+
+Console.Write("ré, "); Console.Beep(1485, 300);
+
+Console.Write("mi, "); Console.Beep(1650, 300);
+
+Console.Write("fá, "); Console.Beep(1759, 300);
+
+Console.Write("fá, "); Console.Beep(1759, 300);
+
+Console.Write("fá, "); Console.Beep(1759, 300);
+
+Thread.Sleep(1000);
+
+ 
+
+Console.Write("Dó, "); Console.Beep(1320, 300);
+
+Console.Write("ré, "); Console.Beep(1485, 300);
+
+Console.Write("Dó, "); Console.Beep(1320, 300);
+
+Console.Write("ré, "); Console.Beep(1485, 300);
+
+Console.Write("ré, "); Console.Beep(1485, 300);
+
+Console.Write("ré, "); Console.Beep(1485, 300);
+
+Thread.Sleep(1000);
+
+ 
+
+Console.Write("Dó, "); Console.Beep(1320, 300);
+
+Console.Write("sol, "); Console.Beep(1980, 300);
+
+Console.Write("fá, "); Console.Beep(1759, 300);
+
+Console.Write("mi, "); Console.Beep(1650, 300);
+
+Console.Write("mi, "); Console.Beep(1650, 300);
+
+Console.Write("mi, "); Console.Beep(1650, 300);
+
+Thread.Sleep(1000);
+
+ 
+
+Console.Write("Dó, "); Console.Beep(1320, 300);
+
+Console.Write("ré, "); Console.Beep(1485, 300);
+
+Console.Write("mi, "); Console.Beep(1650, 300);
+
+Console.Write("fá, "); Console.Beep(1759, 300);
+
+Console.Write("fá, "); Console.Beep(1759, 300);
+
+Console.Write("fá, "); Console.Beep(1759, 300);
